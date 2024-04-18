3122b1b7c8319f5f61f45c9dd53e8c8e6d53a53f
Kimjiho <rlawlgh1204@gmail.com>
2024-04-18 11:24:10 +0900

    commit

---
+++ 01_lecture-source/chap04-view-resolve-lecture-source/src/main/java/com/ohgiraffers/viewresolve/Chap04ViewResolveLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.viewresolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap04ViewResolveLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap04ViewResolveLectureSourceApplication.class, args);
+    }
+
+}
